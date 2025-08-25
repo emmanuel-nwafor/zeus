f3aadc8df443173e737ef73ed2fb95471d636334
emmanuel-nwafor <emitexc.e.o1@gmail.com>
2025-08-25 12:36:33 -0700

    all commit

---
+++ src/lib/db.js
@@ -0,0 +1,23 @@
+// lib/db.js
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  if (isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    throw error; // Throw error to handle in API routes
+  }
+};
